9f8ff28ec1bd9d985bceff842c064da3976534ad
Deeksha-kashyap <47790525+Deeksha-kashyap@users.noreply.github.com>
2019-03-01 23:18:20 +0530

    Create odd_numbers.java

---
+++ odd_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class OddNumber
+{
+   public static void main(String[] args)
+   {
+     System.out.println("Printing the odd numbers");
+     for(int i=1;i<=100;i++)
+     {
+        if(i%2!=0)
+        {
+           System.out.print(i + " ");
+        }
+     }
+   }
+}
